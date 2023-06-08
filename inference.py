812d39b7b7ff0f423315fae170e9fb597afbdee1
seoin0110 <seoinseoinseoin@naver.com>
2023-06-08 08:07:33 +0000

    feat: update inference.py #4

--- inference.py
+++ inference.py
@@ -13,8 +13,8 @@ from datasets.base_dataset import XRayInferenceDataset
 import torch.nn.functional as F
 import constants
 def load_model(saved_model, device):
-    model_name = "model."+ args.model.lower()+ "_custom"
-    model_module = getattr(import_module(model_name), args.model)  
+    model_module_name = "model."+ model_name.lower() + "_custom"
+    model_module = getattr(import_module(model_module_name), model_name)  
     model = model_module().to(device)
 
     model_path = os.path.join(saved_model, "best.pth")
@@ -101,7 +101,7 @@ def inference(data_dir, args):
     df.to_csv(save_path, index=False)
     print(f"Inference Done! Inference result saved at {save_path}")
 
-
+# python inference.py --exp Baseline
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
@@ -110,6 +110,15 @@ if __name__ == "__main__":
     device = torch.device("cuda" if use_cuda else "cpu")
     parser.add_argument("--exp", type=str, default="Baseline", help="exp directory address")
     parser.add_argument("--device", type=str, default=device, help="device (cuda or cpu)")
+    parser.add_argument("--weights", type=str, default="best.pth", help="model weights file (default: best.pth)")
+    parser.add_argument("--batch_size", type=int, default=8, help="input batch size for validing (default: 1000)")
+    parser.add_argument(
+        "--resize", nargs="+", type=int, default=[512, 512], help="resize size for image when you trained (default: [512, 512])"
+    )
+
+    # Container environment
+    parser.add_argument("--data_dir", type=str, default=os.environ.get("SM_CHANNEL_EVAL", "/opt/ml/data"))
+
     args = parser.parse_args()
     exp_path = os.path.join('./outputs',args.exp)
     json_file = next((file for file in os.listdir(exp_path) if file.endswith('.json')), None)
@@ -117,17 +126,7 @@ if __name__ == "__main__":
         json_path = os.path.join(exp_path, json_file)
         with open(json_path, 'r') as f:
             config = json.load(f)
-    parser.add_argument("--batch_size", type=int, default=8, help="input batch size for validing (default: 1000)")
-    parser.add_argument(
-        "--resize", type=tuple, default=(512,512), help="resize size for image when you trained (default: (96, 128))"
-    )
-    parser.add_argument("--model", type=str, default=config["model"], help="model type (default: BaseModel)")
-
-    # Container environment
-    parser.add_argument("--data_dir", type=str, default=os.environ.get("SM_CHANNEL_EVAL", "/opt/ml/data"))
-
-    args = parser.parse_args()
-
+    model_name = config["model"]
     data_dir = args.data_dir
     device = args.device
     inference(data_dir, args)
